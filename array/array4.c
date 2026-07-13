84b688aa377078b72ba52ae0b090155f21410bc2
Chetan Patel <patelchetan1736@gmail.com>
2026-07-13 12:53:47 +0530

    Implement majority element finder in array4.c

---
+++ array/array4.c
@@ -0,0 +1,24 @@
+/**
+Find majority element :
+                    here majority means maximum repeation
+                    
+    INPUT : 1 2 1 1 5 10
+*/
+
+#include<stdio.h>
+int main()
+{
+     int n;
+    printf("Enter size of array : ");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter elements : " );
+    for(int i=0;i<n;i++)
+    {
+      scanf("%d",&a[i]);
+    }
+    int val = a[i];
+    int c1 = 0;
+    int c2 = 0;
+    for(int i = 0;i<n;i++d)
+}
